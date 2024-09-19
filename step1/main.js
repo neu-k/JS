2e46e26076126a72d4263ad22c4e72f67b11ad9f
neu-k <rsk367277@gmail.com>
2024-09-19 15:29:16 +0900

    stopWatch(options)

--- step1/main.js
+++ step1/main.js
@@ -1,1 +1,53 @@
-alert("test");
+function stopWatch(options) {
+  options = options || {};
+  var color = options.color || "lightblue";
+  var backgroundColor = options.backgroundColor || "black";
+  var displayElm = document.getElementsByClassName("display")[0];
+  displayElm.style.color = color;
+  displayElm.style.backgroundColor = backgroundColor;
+
+  function addMessage(message) {
+    var messageElm = document.createElement("div");
+    var now = new Date();
+    messageElm.innerText = 
+      now.getHours() + "時" + 
+      now.getMinutes() + "分" + 
+      now.getSeconds() + "秒" + 
+      message;
+    messageElm.classList = ["message"];
+    logElm.appendChild(messageElm);
+  }
+  
+  var logElm = document.querySelector(".log");
+  var timer = null;
+  var startButton = document.getElementsByClassName("startButton")[0];
+  startButton.addEventListener("click", function() {
+    if (timer === null) {
+      var seconds = 0;
+      timer = 
+        setInterval(function() {
+          seconds++;
+          displayElm.innerText = seconds;
+        }, 1000);
+  
+      addMessage("開始");
+    }
+  });
+  
+  var stopButton = document.getElementsByClassName("stopButton")[0];
+  stopButton.addEventListener("click", function() {
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+  
+      addMessage("終了");
+    }
+  });
+};
+
+var options = {
+  color: "limegreen",
+  backgroundColor: "#333"
+};
+
+stopWatch(options);
